183c91ba1becfd4d7b9a54a6522494fb83f0f7f8
Nikolay Stepanov <nstepanovdev@gmail.com>
2016-11-22 17:49:44 +0300

    init

--- course1/htdocs/index.php
+++ course1/htdocs/index.php
@@ -1,2 +1,21 @@
 <?php
 
+class Product
+{
+    public $var1 = 'var1';
+    protected $var2 = 'var2';
+    private $var3 = 1;
+}
+
+$pr1 = new Product();
+$pr2 = new Product();
+
+$pr1->var1 = 'myvar1';
+//$pr1->var2 = 'myvar2';
+//$pr1->var3 = 'myvar2';
+
+var_dump($pr1);
+var_dump($pr2);
+
+
+
---
+++ course1/htdocs/video1.php
@@ -0,0 +1,14 @@
+<?php
+
+class Product
+{
+}
+
+$pr1 = new Product();
+$pr2 = new Product();
+
+var_dump($pr1);
+var_dump($pr2);
+
+
+
